25bb9d66622e29cef5c4525ef30c6476df6e4077
Hez Ronningen <code@hez.ca>
2021-06-16 15:31:00 -0700

    add to brew dash converter for grain father recipes

--- lib/grain_father/recipe.ex
+++ lib/grain_father/recipe.ex
@@ -1,7 +1,7 @@
 defmodule GrainFather.Recipe do
   @fields ["id", "name", "og", "fg", "abv", "srm", "ibu", "image", "image_url"]
 
-  def from_api(json) do
-    Map.take(json, @fields)
-  end
+  def from_api(json), do: Map.take(json, @fields)
+
+  def to_brew_dash(brew), do: Map.take(brew, ["id", "name"])
 end
