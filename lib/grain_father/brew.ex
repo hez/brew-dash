4f7364ea22ec4a06f96e73bd9cfff8cb4ad84aeb
Hez Ronningen <code@hez.ca>
2021-06-16 15:30:39 -0700

    move abv calc out of grain father in to brew dash

--- lib/grain_father/brew.ex
+++ lib/grain_father/brew.ex
@@ -16,13 +16,13 @@ defmodule GrainFather.Brew do
     json
     |> Map.take(@fields)
     |> Map.update("status", :unknown, &status/1)
-    |> Map.put("abv", abv(json))
     |> Map.put("recipe", GrainFather.Recipe.from_api(recipe))
   end
 
   def to_brew_dash(brew) do
     brew
-    |> Map.take(["original_gravity", "final_gravity", "status", "notes"])
+    |> Map.take(["id", "recipe_id", "original_gravity", "final_gravity", "status", "notes"])
+    |> Map.put("status", status_to_brew_dash(brew["status"]))
     |> Map.put("name", brew["session_name"])
     |> Map.put("brewed_at", brew["created_at"])
     |> Map.put("fermentation_at", brew["fermentation_start_date"])
@@ -35,14 +35,6 @@ defmodule GrainFather.Brew do
   def status(35), do: :serving
   def status(40), do: :complete
 
-  # https://en.wikipedia.org/wiki/Alcohol_by_volume#Practical_estimation_of_alcohol_content
-  def abv(%{"original_gravity" => og, "final_gravity" => fg}) when is_float(og) and is_float(fg),
-    do: 131 * (og - fg)
-
-  def abv(_), do: :unknown
-
-  def abv1(%{"original_gravity" => og, "final_gravity" => fg}) when is_float(og) and is_float(fg),
-    do: 105.0 / 0.79 * (og - fg) / fg
-
-  def abv1(_), do: :unknown
+  def status_to_brew_dash(:complete), do: "completed"
+  def status_to_brew_dash(as_atom), do: Atom.to_string(as_atom)
 end
