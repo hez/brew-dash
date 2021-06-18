a572a563ab0a88c277ad43899ccb303174ba1b15
Hez Ronningen <code@hez.ca>
2021-06-17 17:19:49 -0700

    add context for brews with finders for statuses

---
+++ lib/brew_dash/brews/brew.ex
@@ -0,0 +1,11 @@
+defmodule BrewDash.Brews.Brew do
+  require Ecto.Query
+  alias BrewDash.Repo
+  alias BrewDash.Schema
+
+  def serving, do: all_with_status(:serving)
+  def conditioning, do: all_with_status(:conditioning)
+  def all_with_status(status), do: Schema.Brew |> where_status(status) |> Repo.all()
+
+  def where_status(query, status), do: Ecto.Query.where(query, status: ^status)
+end
