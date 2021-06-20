b6090ce30cd95c495bb74e84d58608355bcef75b
Hez Ronningen <code@hez.ca>
2021-06-20 11:59:19 -0700

    remove the need for the socket being passed in to the image_url func

--- lib/brew_dash_web/live/brew_card_component.ex
+++ lib/brew_dash_web/live/brew_card_component.ex
@@ -4,10 +4,12 @@ defmodule BrewDashWeb.BrewCardComponent do
   alias BrewDash.Schema.Brew
   alias BrewDash.Schema.Recipe
 
-  def image_url(socket, %Brew{recipe: %Recipe{image_url: nil}}),
-    do: Routes.static_path(socket, "/images/default_brew.jpg")
+  @default_image "/images/default_brew.jpg"
 
-  def image_url(_socket, %Brew{recipe: %Recipe{image_url: image_url}}), do: image_url
+  def image_url(%Brew{recipe: %Recipe{image_url: nil}}),
+    do: Routes.static_path(BrewDashWeb.Endpoint, @default_image)
+
+  def image_url(%Brew{recipe: %Recipe{image_url: image_url}}), do: image_url
 
   def show_brew_name?(%Brew{recipe: recipe, name: name}), do: recipe.name != name
 end
