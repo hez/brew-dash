37c40e2ec2c1808d850e69da7501c807c6028618
Graham Baradoy <graham@baradoy.ca>
2021-06-20 11:40:25 -0700

    Add get recipe image_url from grainfather

--- lib/grain_father/recipe.ex
+++ lib/grain_father/recipe.ex
@@ -3,6 +3,14 @@ defmodule GrainFather.Recipe do
 
   def from_api(json), do: Map.take(json, @fields)
 
-  def to_brew_dash(brew),
-    do: brew |> Map.take(["id", "name"]) |> Map.new(fn {k, v} -> {String.to_atom(k), v} end)
+  def to_brew_dash(brew) do
+    brew
+    |> Map.take(["id", "name"])
+    |> Map.new(fn {k, v} -> {String.to_atom(k), v} end)
+    |> Map.put(:image_url, to_brew_dash_image(brew))
+  end
+
+  def to_brew_dash_image(%{"image" => %{"url" => url}}) when is_binary(url), do: url
+  def to_brew_dash_image(%{"image_url" => url}) when is_binary(url), do: url
+  def to_brew_dash_image(_), do: nil
 end
