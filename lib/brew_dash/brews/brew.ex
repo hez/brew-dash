9fa4b837df542b9946dc045d2332e4a2003c7fb2
Hez Ronningen <code@hez.ca>
2021-06-19 17:25:07 -0700

    preload recipe association for all brew all_with_status calls
    Makes it a bit easier on the frontend when ref'ing the brew and its recipe

--- lib/brew_dash/brews/brew.ex
+++ lib/brew_dash/brews/brew.ex
@@ -5,7 +5,11 @@ defmodule BrewDash.Brews.Brew do
 
   def serving, do: all_with_status(:serving)
   def conditioning, do: all_with_status(:conditioning)
-  def all_with_status(status), do: Schema.Brew |> where_status(status) |> Repo.all()
+
+  def all_with_status(status),
+    do: Schema.Brew |> where_status(status) |> load_recipe() |> Repo.all()
 
   def where_status(query, status), do: Ecto.Query.where(query, status: ^status)
+
+  def load_recipe(query), do: Ecto.Query.preload(query, [:recipe])
 end
