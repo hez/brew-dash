4862ec57138dc265ef760c4cdd0b1aa8b804f73f
Hez Ronningen <code@hez.ca>
2021-06-24 15:06:15 -0700

    move display name logic in to helper and handle nil recipe

--- lib/brew_dash_web/live/brew_card_component.ex
+++ lib/brew_dash_web/live/brew_card_component.ex
@@ -6,10 +6,28 @@ defmodule BrewDashWeb.BrewCardComponent do
 
   @default_image "/images/default_brew.jpg"
 
-  def image_url(%Brew{recipe: %Recipe{image_url: nil}}),
+  def image_url(%Brew{recipe: recipe}) when recipe == nil or recipe.image_url == nil,
     do: Routes.static_path(BrewDashWeb.Endpoint, @default_image)
 
   def image_url(%Brew{recipe: %Recipe{image_url: image_url}}), do: image_url
 
-  def show_brew_name?(%Brew{recipe: recipe, name: name}), do: recipe.name != name
+  def display_name(%Brew{} = brew) do
+    name =
+      [display_recipe_name(brew.recipe), display_brew_name(brew)]
+      |> Enum.reject(&is_nil/1)
+      |> Enum.join(" - ")
+
+    [name, display_batch_number(brew)] |> Enum.reject(&is_nil/1) |> Enum.join(" ")
+  end
+
+  def display_recipe_name(nil), do: nil
+  def display_recipe_name(%Recipe{name: name}), do: name
+
+  def display_brew_name(%Brew{recipe: nil, name: nil}), do: "unknown"
+  def display_brew_name(%Brew{recipe: nil, name: name}), do: name
+  def display_brew_name(%Brew{recipe: %_{name: name}, name: name}), do: nil
+  def display_brew_name(%Brew{name: name}), do: name
+
+  def display_batch_number(%Brew{batch_number: bn}) when is_binary(bn), do: "(##{bn})"
+  def display_batch_number(_), do: nil
 end
