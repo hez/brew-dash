dafa9fdab1b8d86f8bb8a2264d4ad47eed8b20e0
Hez Ronningen <code@hez.ca>
2021-07-18 16:15:07 -0700

    Add new helper method to find a recipe by its source id

--- lib/brew_dash/recipes/recipe.ex
+++ lib/brew_dash/recipes/recipe.ex
@@ -1,5 +1,11 @@
 defmodule BrewDash.Recipes.Recipe do
   alias BrewDash.Repo
+  alias BrewDash.Schema
+
+  require Ecto.Query
 
   def upsert!(changeset), do: Repo.insert!(changeset, on_conflict: {:replace_all_except, [:id]})
+
+  def find_by_source_id(source_id),
+    do: Schema.Recipe |> Ecto.Query.where(source_id: ^source_id) |> Repo.one()
 end
