dff825cd0891f7a84037568a28a61ff39dbeb936
Hez Ronningen <code@hez.ca>
2021-07-26 17:03:03 -0700

    Add display of tap number to the brew card component

--- lib/brew_dash_web/live/brew_card_component.ex
+++ lib/brew_dash_web/live/brew_card_component.ex
@@ -34,4 +34,17 @@ defmodule BrewDashWeb.BrewCardComponent do
   def status_badge(%Brew{status: :serving}), do: "ON TAP"
   def status_badge(%Brew{status: :conditioning}), do: "ON DECK"
   def status_badge(%Brew{status: status}), do: status |> to_string() |> String.upcase()
+
+  def tap_number_present?(%Brew{status: :serving, tap_number: tap_number})
+      when is_binary(tap_number),
+      do: String.length(tap_number) > 0
+
+  def tap_number_present?(brew), do: false
+
+  def tap_number_badge(%Brew{tap_number: tap_number}) do
+    case Integer.parse(tap_number) do
+      {number, ""} -> "Tap #{number}"
+      _ -> tap_number
+    end
+  end
 end
