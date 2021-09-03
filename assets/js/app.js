0b5f69347cee2769ddc5af10fd53e81088272bb4
Hez Ronningen <code@hez.ca>
2021-09-03 09:49:30 -0700

    update js and css
    - move to css instead of scss
    - statically include bootstrap and theme

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import "../css/app.css"
 
-import "bootstrap"
+import "./bootstrap/bootstrap.js"
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
