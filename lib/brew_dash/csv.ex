d57469c32a4e54e07e448311865ecf9b85e5f27f
Hez Ronningen <code@hez.ca>
2021-10-31 13:38:44 -0700

    add some more logging for error states

--- lib/brew_dash/csv.ex
+++ lib/brew_dash/csv.ex
@@ -1,4 +1,5 @@
 defmodule BrewDash.CSV do
+  require Logger
   alias BrewDash.CSV
 
   @spec sync_recipes(String.t()) :: {:ok, integer()} | {:error, String.t()}
@@ -6,7 +7,9 @@ defmodule BrewDash.CSV do
     results = CSV.Recipe.sync!(path)
     {:ok, Enum.count(results)}
   rescue
-    _error -> {:error, "error"}
+    error ->
+      Logger.error("error parsing recipes #{inspect(error)}")
+      {:error, "error"}
   end
 
   @spec sync_brew_sessions(String.t()) :: {:ok, integer()} | {:error, String.t()}
@@ -14,6 +17,8 @@ defmodule BrewDash.CSV do
     results = CSV.Brew.sync!(path)
     {:ok, Enum.count(results)}
   rescue
-    _error -> {:error, "error"}
+    error ->
+      Logger.error("error parsing brew sessions #{inspect(error)}")
+      {:error, "error"}
   end
 end
--- lib/brew_dash/csv/brew.ex
+++ lib/brew_dash/csv/brew.ex
@@ -1,4 +1,5 @@
 defmodule BrewDash.CSV.Brew do
+  require Logger
   alias BrewDash.Brews
   alias BrewDash.Recipes
   alias BrewDash.Schema.Brew
@@ -34,6 +35,8 @@ defmodule BrewDash.CSV.Brew do
     |> File.stream!()
     |> CSV.decode!(headers: true)
     |> Enum.map(fn line ->
+      Logger.debug("parsing csv line: #{inspect(line)}")
+
       %{
         @default
         | batch_number: line["batch_number"],
@@ -64,6 +67,7 @@ defmodule BrewDash.CSV.Brew do
   def status("conditioning"), do: :conditioning
   def status("serving"), do: :serving
   def status("completed"), do: :completed
+
   def status(status) do
     Logger.error("Illegal status value of #{inspect(status)}")
     :planning
