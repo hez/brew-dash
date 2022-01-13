3b3e458e5b8b20a55213f4987ab6c0209d9c94e2
Hez Ronningen <code@hez.ca>
2022-01-12 17:40:30 -0800

    disabling recipe update notifications until it is implemented
    Was causing lots of error messages.

--- lib/brew_dash_web/live/admin/recipe_edit_live.ex
+++ lib/brew_dash_web/live/admin/recipe_edit_live.ex
@@ -12,7 +12,8 @@ defmodule BrewDashWeb.Admin.RecipeEditLive do
   @impl true
   def handle_event("delete", %{"value" => id}, socket) do
     Recipe.delete!(id)
-    BrewDash.Sync.broadcast(:brew_sessions, :recipe_updated)
+    # TODO implement handling
+    # BrewDash.Sync.broadcast(:brew_sessions, :recipe_updated)
 
     socket =
       socket
@@ -29,7 +30,8 @@ defmodule BrewDashWeb.Admin.RecipeEditLive do
 
   def handle_event("save", _params, %{assigns: %{live_action: :new}} = socket) do
     recipe = Recipe.insert!(socket.assigns.changeset)
-    BrewDash.Sync.broadcast(:brew_sessions, :recipe_updated)
+    # TODO implement handling
+    # BrewDash.Sync.broadcast(:brew_sessions, :recipe_updated)
 
     socket =
       socket
@@ -42,7 +44,8 @@ defmodule BrewDashWeb.Admin.RecipeEditLive do
 
   def handle_event("save", %{"recipe" => recipe}, socket) do
     recipe = Recipe.update!(socket.assigns.recipe, recipe)
-    BrewDash.Sync.broadcast(:brew_sessions, :recipe_updated)
+    # TODO implement handling
+    # BrewDash.Sync.broadcast(:brew_sessions, :recipe_updated)
 
     socket =
       socket
