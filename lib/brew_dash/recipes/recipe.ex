4e10b127b8e1e9ccfb71628b92169f7ff79f0f31
Hez Ronningen <code@hez.ca>
2022-01-12 17:13:14 -0800

    add recipe crud functions

--- lib/brew_dash/recipes/recipe.ex
+++ lib/brew_dash/recipes/recipe.ex
@@ -6,7 +6,15 @@ defmodule BrewDash.Recipes.Recipe do
 
   def all, do: Repo.all(Schema.Recipe)
 
+  def get!(id), do: Repo.get!(Schema.Recipe, id)
+
+  def insert!(changeset), do: Repo.insert!(changeset)
+
   def upsert!(changeset, fields), do: Repo.insert!(changeset, on_conflict: {:replace, fields})
+
+  def update!(recipe, attrs), do: recipe |> Schema.Recipe.changeset(attrs) |> Repo.update!()
+
+  def delete!(id), do: Schema.Recipe |> Repo.get!(id) |> Repo.delete()
 
   def find_by_source_id(source_id),
     do: Schema.Recipe |> Ecto.Query.where(source_id: ^source_id) |> Repo.one()
