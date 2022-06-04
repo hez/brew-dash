db56968c40d0d7d194f42860602933c7ca6484bd
Hez Ronningen <code@hez.ca>
2022-06-04 13:39:27 -0700

    csv import tooling for bottles

--- lib/brew_dash/bottles/bottle.ex
+++ lib/brew_dash/bottles/bottle.ex
@@ -3,7 +3,9 @@ defmodule BrewDash.Bottles.Bottle do
   alias BrewDash.Repo
   alias BrewDash.Schema
 
-  def all, do: Schema.Bottle |> Ecto.Query.order_by(desc: :purchased_at) |> Repo.all()
+  def all(filter \\ [], sort \\ [:company, :name]),
+    do: Schema.Bottle |> Ecto.Query.where(^filter) |> Ecto.Query.order_by(^sort) |> Repo.all()
+
   def get!(id), do: Repo.get!(Schema.Bottle, id)
 
   @spec upsert!(Ecto.Changeset.t(), list(atom())) :: Ecto.Schema.t()
