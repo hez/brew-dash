34ad7c0fd9bda68086a55e023bc15e8029ecbb17
Hez Ronningen <code@hez.ca>
2022-06-30 11:20:41 -0700

    refactor menu items in to helper
    Tidies up the heex

--- lib/brew_dash_web/live/admin/menu_component.ex
+++ lib/brew_dash_web/live/admin/menu_component.ex
@@ -8,4 +8,16 @@ defmodule BrewDashWeb.Admin.MenuComponent do
     Tasks.SyncGrainFatherServer.sync_now(true)
     {:noreply, put_flash(socket, :info, "Sync manually triggered.")}
   end
+
+  def menu_item(%{label: label, link: link} = assigns) do
+    ~H"""
+    <li>
+      <%= live_redirect(label,
+        to: link,
+        class:
+          "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+      ) %>
+    </li>
+    """
+  end
 end
