b2ffabbfd8887861da4eee804a0eecd39bcb8555
Hez Ronningen <code@hez.ca>
2022-07-09 09:51:18 -0700

    add icon to recipe if it has been archived

--- lib/brew_dash_web/live/admin/recipes_list_live.ex
+++ lib/brew_dash_web/live/admin/recipes_list_live.ex
@@ -8,5 +8,15 @@ defmodule BrewDashWeb.Admin.RecipesListLive do
     {:ok, fetch_recipes(socket)}
   end
 
+  def archived_icon(%{recipe: %{is_archived: true}} = assigns) do
+    ~H"""
+    <div class="pr-3">
+      <.icon_with_tool_tip tip="Recipe has been archived" image="delete.svg" />
+    </div>
+    """
+  end
+
+  def archived_icon(assigns), do: ~H""
+
   defp fetch_recipes(socket), do: assign(socket, recipes: Recipe.all())
 end
