2fec2978329256de81f18089d22399ceb91b9b8f
Hez Ronningen <code@hez.ca>
2023-08-30 11:14:19 -0700

    refactor to use req instead of tesla
    - introduced new grain_father/session module

--- lib/brew_dash/tasks/sync_grain_father.ex
+++ lib/brew_dash/tasks/sync_grain_father.ex
@@ -7,34 +7,24 @@ defmodule BrewDash.Tasks.SyncGrainFather do
   @grain_father_page_limit 1
 
   def sync!(limit \\ @grain_father_page_limit) do
-    {:ok, token} = login()
-    sync_recipes!(token, limit)
-    sync_brew_sessions!(token, limit)
+    {:ok, session} = GrainFather.new_session()
+    sync_recipes!(session, limit)
+    sync_brew_sessions!(session, limit)
   end
 
-  defp login, do: GrainFather.login()
-
-  def sync_recipes!(token, limit) do
-    token
-    |> GrainFather.fetch_all(limit, :recipes)
-    |> Enum.each(&write_recipe!/1)
-  end
+  def sync_recipes!(session, limit),
+    do: session |> GrainFather.fetch_all_recipes(limit) |> Enum.each(&write_recipe!/1)
 
   def write_recipe!(recipe) do
-    attrs =
-      recipe
-      |> GrainFather.Recipe.from_api()
-      |> GrainFather.Recipe.to_brew_dash()
+    attrs = recipe |> GrainFather.Recipe.from_api() |> GrainFather.Recipe.to_brew_dash()
 
     %Recipe{}
     |> Recipe.source_changeset(attrs)
     |> Recipes.Recipe.upsert!(GrainFather.Recipe.brew_dash_fields())
   end
 
-  def sync_brew_sessions!(token, limit) do
-    token
-    |> GrainFather.fetch_all(limit, :brew_sessions)
-    |> Enum.each(&write_brew_session!/1)
+  def sync_brew_sessions!(session, limit) do
+    session |> GrainFather.fetch_all_brew_sessions(limit) |> Enum.each(&write_brew_session!/1)
 
     BrewDash.Sync.broadcast(:brew_sessions, :brew_sessions_updated)
   end
@@ -42,10 +32,7 @@ defmodule BrewDash.Tasks.SyncGrainFather do
   def write_brew_session!(brew) do
     recipe_id = brew["recipe_id"] |> to_string |> recipe_id_for()
 
-    attrs =
-      brew
-      |> GrainFather.Brew.from_api()
-      |> GrainFather.Brew.to_brew_dash()
+    attrs = brew |> GrainFather.Brew.from_api() |> GrainFather.Brew.to_brew_dash()
 
     %Brew{recipe_id: recipe_id}
     |> Brew.source_changeset(attrs)
