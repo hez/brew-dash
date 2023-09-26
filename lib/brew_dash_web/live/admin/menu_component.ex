d9957a9db411e142ec366fc2e0b1fb8de1c7ed96
Hez Ronningen <code@hez.ca>
2023-09-26 09:54:40 -0700

    use .link instead of live_redirect, since it was deprecated

--- lib/brew_dash_web/live/admin/menu_component.ex
+++ lib/brew_dash_web/live/admin/menu_component.ex
@@ -15,11 +15,12 @@ defmodule BrewDashWeb.Admin.MenuComponent do
   def menu_item(assigns) do
     ~H"""
     <li>
-      <%= live_redirect(@label,
-        to: @link,
-        class:
-          "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-      ) %>
+      <.link
+        href={@link}
+        class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+      >
+        <%= @label %>
+      </.link>
     </li>
     """
   end
