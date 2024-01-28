f5350f0b6c66dea37b0ed168b3e719b04aac0627
Hez Ronningen <code@hez.ca>
2024-01-28 15:35:19 -0800

    add history link and move template in to the live controller

--- lib/brew_dash_web/live/dashboard_live.ex
+++ lib/brew_dash_web/live/dashboard_live.ex
@@ -3,4 +3,28 @@ defmodule BrewDashWeb.DashboardLive do
 
   @impl true
   def mount(_params, _session, socket), do: {:ok, socket}
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div class="full-w p-8">
+      <ul class="px-8">
+        <li><.link patch={~p"/taps"} class="py-2 text-blue-700">On Tap</.link></li>
+        <li><.link patch={~p"/history"} class="py-2 text-blue-700">Brew History</.link></li>
+        <li><.link patch={~p"/bottles"} class="py-2 text-blue-700">Bottles in Cellar</.link></li>
+      </ul>
+    </div>
+
+    <div class="full-w p-8">
+      <h1 class="font-bold text-2xl">About</h1>
+      <div class="py-2">
+        BrewDash is a quick way to sync your GrainFather brew history to a convient dashboard.
+        You can find the source code at
+        <.link navigate="https://github.com/hez/brew_dash" class="py-2 text-blue-700">
+          github.com/hez/brew_dash
+        </.link>
+      </div>
+    </div>
+    """
+  end
 end
