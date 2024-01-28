b3adcd4cc2b511e1f6e6cfaf0377d7d8ef539f89
Hez Ronningen <code@hez.ca>
2024-01-28 15:35:49 -0800

    move history template in to the live controller and refactor

--- lib/brew_dash_web/live/history_live.ex
+++ lib/brew_dash_web/live/history_live.ex
@@ -2,10 +2,61 @@ defmodule BrewDashWeb.HistoryLive do
   use BrewDashWeb, :live_view
   import BrewDash.Brews.Display
   alias BrewDash.Brews.Brew
+  alias BrewDash.Schema
 
   @impl true
   def mount(_params, _session, socket) do
     {:ok, fetch_brew_sessions(socket)}
+  end
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div class="container">
+      <h2>Brew Session History</h2>
+      <table class="table table-sm table-bordered table-striped">
+        <thead>
+          <tr class="table-primary">
+            <th scope="col">Brewed</th>
+            <th scope="col">Batch</th>
+            <th scope="col">Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <.years_brew_sessions
+            :for={brews <- @brew_sessions}
+            brews={brews}
+            first={first_with_brewed_at(brews)}
+          />
+        </tbody>
+      </table>
+    </div>
+    """
+  end
+
+  attr :brews, :list, required: true
+  attr :first, Schema.Brew, default: nil
+
+  def years_brew_sessions(assigns) do
+    ~H"""
+    <tr :if={@first}>
+      <th><%= @first.brewed_at.year %></th>
+      <th></th>
+      <th></th>
+    </tr>
+
+    <tr :for={brew <- @brews}>
+      <td class="ps-4">
+        <%= brew |> brewed_at!() |> month_name() %> <%= brew |> brewed_at!() |> Map.get(:day) %>
+      </td>
+      <td>
+        <%= brew.batch_number %>
+      </td>
+      <td>
+        <%= name(brew) %>
+      </td>
+    </tr>
+    """
   end
 
   defp fetch_brew_sessions(socket) do
@@ -18,6 +69,8 @@ defmodule BrewDashWeb.HistoryLive do
     assign(socket, brew_sessions: brews_by_year)
   end
 
+  def month_name(%{month: month}), do: month_name(month)
+  def month_name(nil), do: nil
   def month_name(1), do: "Jan"
   def month_name(2), do: "Feb"
   def month_name(3), do: "Mar"
@@ -30,4 +83,6 @@ defmodule BrewDashWeb.HistoryLive do
   def month_name(10), do: "Oct"
   def month_name(11), do: "Nov"
   def month_name(12), do: "Dec"
+
+  defp first_with_brewed_at(brews), do: Enum.find(brews, fn brew -> brew.brewed_at != nil end)
 end
