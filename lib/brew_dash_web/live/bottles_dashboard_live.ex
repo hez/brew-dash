30d63ff287ac0c962d79a7a29224baf0e8a3ca43
Hez Ronningen <code@hez.ca>
2024-02-03 15:02:23 -0800

    move bottles template in to the controller

--- lib/brew_dash_web/live/bottles_dashboard_live.ex
+++ lib/brew_dash_web/live/bottles_dashboard_live.ex
@@ -6,4 +6,16 @@ defmodule BrewDashWeb.BottlesDashboardLive do
   def mount(params, _session, socket) do
     {:ok, assign(socket, filter: params)}
   end
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <.live_component
+      module={BrewDashWeb.BottlesListComponent}
+      id="bottles_list"
+      filter={@filter}
+      render_actions="true"
+    />
+    """
+  end
 end
